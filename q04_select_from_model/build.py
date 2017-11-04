cf80be7fd510fa6ec81e92ee1fd9fb1122587c5a
samir0719 <sameer_0719@yahoo.com>
2017-11-04 05:22:17 +0000

    Done

--- q04_select_from_model/build.py
+++ q04_select_from_model/build.py
@@ -1,3 +1,4 @@
+# %load q04_select_from_model/build.py
 # Default imports
 from sklearn.feature_selection import SelectFromModel
 from sklearn.ensemble import RandomForestClassifier
@@ -5,6 +6,14 @@ import pandas as pd
 import numpy as np
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
-
-
+def select_from_model(data):
+    x=data.iloc[:,:-1]
+    y=data.iloc[:,-1]
+    np.random.seed(9)
+    mod=RandomForestClassifier()
+    z=SelectFromModel(mod)
+    z.fit(x,y)
+    a=x.columns.values
+    b=a[z.get_support()].tolist()
 # Your solution code here
+    return b
